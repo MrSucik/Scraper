2f46dc9ce1dd32de6aa25d0b84b218c3ab3a062a
Daniel <mr.sucik@gmail.com>
2019-05-05 20:55:23 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Scraper
+{
+    class Program
+    {
+        private static string urlFirst = "<a class=\"business-name\" href=\"";
+        private static string urlSecond = "\" data-analytics=";
+        private static readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Scraper Init!");
+            var baseUrl = ConfigurationManager.AppSettings.Get("baseUrl");
+            var outputPath = ConfigurationManager.AppSettings.Get("outputPath");
+            var search = ConfigurationManager.AppSettings.Get("search");
+            var timeout = Convert.ToInt32(ConfigurationManager.AppSettings.Get("timeout"));
+            Console.WriteLine("Config:");
+            Console.WriteLine("Base URL: " + baseUrl);
+            Console.WriteLine("Output Path " + outputPath);
+            Console.WriteLine("Search query string: " + search);
+            Console.WriteLine("Timeout between requests: " + timeout);
+            var page = 1;
+            while (true)
+            {
+                Console.WriteLine("Starting new page...");
+                var response = await client.GetAsync(baseUrl + search + page.ToString(), HttpCompletionOption.ResponseContentRead);
+                if (!response.IsSuccessStatusCode)
+                    throw new Exception("Request failed!");
+                var content = await response.Content.ReadAsStringAsync();
+                var urls = GetUrls(content);
+                var records = new List<Record>();
+                foreach (var url in urls)
+                {
+                    if (timeout > 0)
+                        Thread.Sleep(timeout);
+                    var res = await client.GetAsync(baseUrl + url, HttpCompletionOption.ResponseContentRead);
+                    var data = await res.Content.ReadAsStringAsync();
+                    records.Add(new Record(data));
+                }
+                Console.WriteLine("Saving records...");
+                var prevContent = File.Exists(outputPath)
+                    ? JsonConvert.DeserializeObject<List<Record>>(await File.ReadAllTextAsync(outputPath))
+                    : new List<Record>();
+                var newContent = prevContent.Union(records);
+                await File.WriteAllTextAsync(outputPath, JsonConvert.SerializeObject(newContent), Encoding.UTF8);
+                Console.WriteLine("Saving complete!");
+                Console.WriteLine($"Total records: {newContent.Count()}");
+                page += 1;
+            }
+        }
+        private static List<string> GetUrls(string html)
+        {
+            var parts = html.Split(urlFirst);
+            var urls = new List<string>();
+            foreach (var item in parts)
+            {
+                if (!item.Contains(urlSecond))
+                    continue;
+                var url = item.Split(urlSecond)[0];
+                if (url.Length > 100)
+                    continue;
+                urls.Add(url);
+            }
+            return urls;
+        }
+    }
+}
---
+++ Record.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Scraper
+{
+    class Record
+    {
+        private readonly string urlFirst = "<a class=\"secondary-btn website-link\" href=\"";
+        private readonly string nameFirst = "<div class=\"sales-info\"><h1>";
+        private readonly string nameSecond = "<";
+        private readonly string mailFirst = "<a class=\"email-business\" href=\"mailto:";
+        private readonly Regex mailRegex = new Regex("<a class=\"email-business\" href=\"mailto:[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\\.[a-zA-Z0-9._-]+\"");
+        private readonly string UnknownPlaceholder = "Unknown";
+        public Record(string data)
+        {
+            try
+            {
+                Name = data.Split(nameFirst)[1].Split(nameSecond)[0];
+            }
+            catch
+            {
+                Name = UnknownPlaceholder;
+            }
+            try
+            {
+                SiteUrl = data.Split(urlFirst)[1].Split('"')[0];
+            }
+            catch
+            {
+                SiteUrl = UnknownPlaceholder;
+            }
+            try
+            {
+                Email = mailRegex.IsMatch(data)
+                    ? mailRegex.Match(data).Value.Replace(mailFirst, string.Empty).Replace("\"", string.Empty)
+                    : UnknownPlaceholder;
+            }
+            catch
+            {
+                Email = UnknownPlaceholder;
+            }
+
+        }
+
+        public string Name { get; set; }
+        public string SiteUrl { get; set; }
+        public string Email { get; set; }
+    }
+}
